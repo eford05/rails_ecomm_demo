025ba774884cbd977b1dc25adf8f940f4574de31
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-27 11:49:12 -0500

    Redirect admins to admin homepage, set admin layout for logged in admin, add logout to admin dash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  layout :set_layout
+
+  # Use admin layout for logged in admins
+  def set_layout
+    if admin_signed_in?
+      "admin"
+    else
+      "application"
+    end
+  end
+
+  # Redirect after admin login to admin root path
+  def after_sign_in_path_for(resource)
+    if admin_signed_in?
+      stored_location_for(resource) || admin_path
+    end
+  end
 end
