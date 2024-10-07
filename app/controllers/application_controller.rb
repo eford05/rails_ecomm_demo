ff1dd422e3bf281027248caf210a950d95af39fa
eford05 <89802740+eford05@users.noreply.github.com>
2024-10-07 13:22:03 -0500

    Generate Category scaffold, fix links to category paths, update class names for devise forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  layout :set_layout
-
-  # Use admin layout for logged in admins
-  def set_layout
-    if admin_signed_in?
-      "admin"
-    else
-      "application"
-    end
-  end
-
   # Redirect after admin login to admin root path
   def after_sign_in_path_for(resource)
     if admin_signed_in?
